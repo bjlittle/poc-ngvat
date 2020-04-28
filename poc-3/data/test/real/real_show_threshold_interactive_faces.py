d111bf969a30f78e8bc9cdd003c438af39790dee
Bill Little <bill.james.little@gmail.com>
2020-04-28 13:32:08 +0100

    tidy

--- poc-3/data/test/real/real_show_threshold_interactive_faces.py
+++ poc-3/data/test/real/real_show_threshold_interactive_faces.py
@@ -3,26 +3,14 @@ import numpy as np
 import pyvista as pv
 
 
-sst = pv.read("pdata_xy_sst_t0.vtk")
-
+sst = pv.read("pdata_sst_t0.vtk")
 cmap = "coolwarm"  # colorcet (perceptually accurate) color maps
-sargs = dict(
-             shadow=True,
-             n_labels=5,
-             italic=False,
-             fmt="%.1f",
-             font_family="courier",
-#             nan_annotation=True,
-             vertical=True,
-)
 
 p = pv.BackgroundPlotter()
-actor = p.add_mesh_threshold(sst, scalars="faces", invert=True, title="SST / K", show_edges=True, cmap=cmap, scalar_bar_args=sargs, show_scalar_bar=True)
-p.add_text("C48 Sea Surface Temperature", font_size=10, shadow=True, font="courier")
+actor = p.add_mesh_threshold(sst, scalars="faces", invert=True, title="SST / K", show_edges=True, cmap=cmap)
+p.add_text("C48 SST (cell threshold)", font_size=10, shadow=True, font="courier")
 
 p.show_axes()
-#p.show_grid()
-p.scalar_bar.SetTitle("sst / K")
-#p.add_scalar_bar(**sargs)
+p.scalar_bar.SetTitle("SST / K")
 p.camera_position = "yz"
 
