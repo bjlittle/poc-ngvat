a875652c8fb11c778a2d8baae9e4520785a68d03
Bill Little <bill.james.little@gmail.com>
2020-05-01 05:10:48 +0100

    update

--- poc-3/candy_cell_march.py
+++ poc-3/candy_cell_march.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pyvista as pv
 
+from utils import coastline
+
 
 def button_stop(status):
     global stop
@@ -51,11 +53,10 @@ def event_draw():
                 run = False
 
 
-def add_text(p, depth=0.1):
+def add_text(p, depth=0.1, opacity=1.0, color="tan"):
     x, y, z = 0, 1, 2
     scale = 0.5
     offset = 1.0 + depth/2
-    color = "tan"
     
     def center(pts, scale):
         pts[:, x] -= np.min(pts[:, x])
@@ -75,7 +76,7 @@ def add_text(p, depth=0.1):
     t.rotate_x(90)
     t.rotate_z(90)
     t.translate((offset, 0, 0))
-    p.add_mesh(t, color=color)
+    p.add_mesh(t, color=color, opacity=opacity)
     
     # panel: 2
     t = pv.Text3D("2", depth=depth)
@@ -84,7 +85,7 @@ def add_text(p, depth=0.1):
     t.rotate_x(90)
     t.rotate_z(180)
     t.translate((0,  offset, 0))
-    p.add_mesh(t, color=color)
+    p.add_mesh(t, color=color, opacity=opacity)
     
     # panel: 3
     t = pv.Text3D("3", depth=depth)
@@ -101,7 +102,7 @@ def add_text(p, depth=0.1):
     center(pts, scale)
     t.rotate_x(90)
     t.translate((0, -offset, 0))
-    p.add_mesh(t, color=color)
+    p.add_mesh(t, color=color, opacity=opacity)
 
     # panel: 5
     t = pv.Text3D("5", depth=depth)
@@ -109,7 +110,7 @@ def add_text(p, depth=0.1):
     center(pts, scale)
     t.rotate_z(180)
     t.translate((0, 0, offset))
-    p.add_mesh(t, color=color)
+    p.add_mesh(t, color=color, opacity=opacity)
 
     # panel: 6
     t = pv.Text3D("6", depth=depth)
@@ -117,7 +118,7 @@ def add_text(p, depth=0.1):
     center(pts, scale)
     t.rotate_y(180)
     t.translate((0, 0, -offset))
-    p.add_mesh(t, color=color)
+    p.add_mesh(t, color=color, opacity=opacity)
     
         
 mesh = pv.read("data/test/synthetic/pdata_C12_synthetic.vtk")
@@ -133,13 +134,18 @@ stop = False
 clim=(np.min(mesh["faces"]), np.max(mesh["faces"]))
 add = True
 
+coastlines = coastline(resolution="110m")
+
 p = pv.BackgroundPlotter()
 p.add_mesh(mesh, scalars=None, opacity=0, show_scalar_bar=False)
 p.add_text("C12 Panel Cell Order", font_size=13, shadow=True, font="courier")
-add_text(p, depth=0.1)
+add_text(p, depth=0.1, opacity=0.7)
 p.add_bounding_box()
 actor_run = p.add_checkbox_button_widget(button_run, value=False, size=20, border_size=2, color_on="green")
 p.add_checkbox_button_widget(button_stop, value=False, size=20, border_size=2, color_on="green", position=(10, 33))
+
+for geometry in coastlines:
+    p.add_mesh(geometry, color="white")
 
 rep_run = actor_run.GetRepresentation()
 
